6a3fad6d06edb141070497f5776d85995d43e71f
Tulir Asokan <tulir@maunium.net>
2020-07-09 16:58:36 +0300

    Update mautrix-python

--- mautrix_hangouts/db/__init__.py
+++ mautrix_hangouts/db/__init__.py
@@ -5,11 +5,6 @@ from .portal import Portal
 from .puppet import Puppet
 from .user import User, UserPortal, Contact
 
-try:
-    from mautrix.bridge.db.nio_state_store import init as init_nio_db
-except ImportError:
-    init_nio_db = None
-
 
 def init(db_engine) -> None:
     for table in Portal, Message, User, Puppet, UserProfile, RoomState, UserPortal, Contact:
@@ -17,5 +12,3 @@ def init(db_engine) -> None:
         table.t = table.__table__
         table.c = table.t.c
         table.column_names = table.c.keys()
-    if init_nio_db:
-        init_nio_db(db_engine)
