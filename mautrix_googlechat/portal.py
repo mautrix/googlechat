933a2b5f5b2ded4224a678d8a06d2370a6f9b7f4
Tulir Asokan <tulir@maunium.net>
2022-12-16 22:08:56 +0200

    Fix formatting

--- mautrix_googlechat/portal.py
+++ mautrix_googlechat/portal.py
@@ -804,9 +804,7 @@ class Portal(DBPortal, BasePortal):
         reply_to = await DBMessage.get_by_mxid(
             message.get_thread_parent() or message.get_reply_to(), self.mxid
         )
-        thread_id = (
-            (reply_to.gc_parent_id or reply_to.gcid) if reply_to else None
-        )
+        thread_id = (reply_to.gc_parent_id or reply_to.gcid) if reply_to else None
         local_id = f"mautrix-googlechat%{random.randint(0, 0xffffffffffffffff)}"
         self._local_dedup.add(local_id)
 
